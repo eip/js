46a792938c424d445b650e1ec7872b8a0b1feffe
Ed Popsuev <ed.popsuev@gmail.com>
2012-05-27 14:32:21 +0300

    fontBuddy smart sorting & refactoring

--- fontbuddy.js
+++ fontbuddy.js
@@ -1,5 +1,5 @@
 var fontBuddy = {
-  getStyle: function(elm, style) {
+  getElmStyle: function(elm, style) {
     var res = "";
     if (document.defaultView && document.defaultView.getComputedStyle) {
       res = document.defaultView.getComputedStyle(elm, "").getPropertyValue(style);
@@ -11,21 +11,49 @@ var fontBuddy = {
     }
     return res;
   },
-  listFonts: function() {
-    var elms = document.all;
+  getFontStyle: function(elm) {
+    return this.getElmStyle(elm, "font-family") + ' ' + this.getElmStyle(elm, "font-size");
+  },
+  splitFontStyle: function(fs) {
+    var i = fs.lastIndexOf(' ');
+    var nm = fs.substring(0, i).replace(/'/g, '').toLowerCase();
+    var sz = fs.substring(i + 1).replace(/\D+/, '') * 1;
+    return {
+      'name': nm,
+      'size': sz
+    };
+  },
+  sortFonts: function(fonts) {
+    var fsrt = {};
+    var farr = [];
+    for (var fs in fonts) {
+      farr.push(fs);
+    }
+    farr.sort(function(a, b) {
+      var af = fontBuddy.splitFontStyle(a);
+      var bf = fontBuddy.splitFontStyle(b);
+      return af.name < bf.name ? -1 : (af.name > bf.name ? 1 : (af.size - bf.size));
+    });
+    for (var i = 0; i < farr.length; i++) {
+      fsrt[farr[i]] = fonts[farr[i]];
+    }
+    return fsrt;
+  },
+  getFonts: function(sorted) {
     var fonts = {};
+    var elms = document.all;
+    var s = '';
     for (var i = 0; i < elms.length; i++) {
-      s = this.getStyle(elms[i], "font-family") + ' ' + this.getStyle(elms[i], "font-size");
+      s = this.getFontStyle(elms[i]);
       fonts[s] = (fonts[s] ? fonts[s] : 0) + 1;
     };
-    var fa = [];
+    return sorted ? this.sortFonts(fonts) : fonts;
+  },
+  printFonts: function() {
+    var fonts = this.getFonts(1);
     for (var fs in fonts) {
-      fa.push(fs);
-    }
-    fa.sort();
-    for (var i = 0; i < fa.length; i++) {
-      console.log(fa[i] + ' -> ' + fonts[fa[i]] + ' elements');
+      console.log(fs + ' -> ' + fonts[fs] + ' elements');
     }
   }
 }
-fontBuddy.listFonts();
+fontBuddy.printFonts();
